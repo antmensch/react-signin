da5c7911c8adc509a4847e6c9a9995c103a0d870
antmensch <105740241+antmensch@users.noreply.github.com>
2022-06-26 16:28:04 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
+import React, {useState, useRef} from "react";
 import './App.css';
+import LoginForm from './components/loginForm/LoginForm';
 
 function App() {
+  const adminUser = {
+      email: "admin@admin.com",
+      password: "admin123"
+  }
+
+  const [user, setUser] = useState({
+      email: "",
+      name: "",
+      birthDate: "",
+      gender: "",
+      password: ""
+  });
+
+  const [error, setError] = useState("");
+
+  function signup(details) {
+      setUser(prev => ({
+        ...prev,
+        email: details.email,
+        name: details.name,
+        birthDate: details.birthDate,
+        gender: details.gender,
+        password: details.password,
+      }));
+  }
+
+  function logout() {
+    setUser({
+      email: "",
+      name: "",
+      birthDate: "",
+      gender: "",
+      password: ""
+  })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className='App'>
+      {(user.email != "") ? (
+        <div className="welcome">
+          <h2 className="welcome--heading">Hi there, <span className="welcome--userName">{user.name}</span>! Thanks for signing up!</h2>
+          <button className="welcome--button" onClick={logout}>Back</button>
+        </div>
+      ) : (
+        <LoginForm signup={signup} error={error} />
+      )}
+      
+    </main>
+    
   );
 }
 
